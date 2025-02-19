93f1ae8bb1be2d73f4f6bdf33ee281bbed728474
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-02-19 12:27:03 +0300

    Changes

--- pride/settings.py
+++ pride/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pe&i#efuhp3rs==7%_rnwxl!%!hp%ynk@n9p3t7orq9%_-*a_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["pride-nnze.onrender.com"]
 
